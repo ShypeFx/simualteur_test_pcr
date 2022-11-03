eba13a322b4e3f7f6404e0263cfb06c289064b27
Tristan BEAUDOIN <tristan.beaudoin@gmail.com>
2022-11-03 23:47:23 +0100

    fix all issues, need clean code

--- main/src/Main.java
+++ main/src/Main.java
@@ -1,5 +1,12 @@
+import java.sql.SQLException;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+
         System.out.println("Hello world!");
+
+        DataBaseConnect db = new DataBaseConnect();
+        db.Check_ID("000000");
+
     }
 }
